4c8ff6dde5baaabcf9e9021fcd88f4be7219ef82
shoumik011 <shoumikahmed01@gmail.com>
2024-12-01 16:04:46 -0500

    created the server and used GET, POST, PATCH to modify the library

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"errors"
 )
 
 type book struct {
@@ -23,8 +25,74 @@ func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+func bookById(c *gin.Context){
+	id:= c.Param("id")
+	book, err:= getBookById(id)
+	if err!=nil{
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message":"Book not found."})
+		return
+	}
+	c.IndentedJSON(http.StatusOK,book)
+}
+func getBookById(id string)(*book,error){
+	for i, b := range books{
+		if b.ID ==id{
+			return &books[i], nil
+		}
+	}
+	return nil, errors.New("book not found") 
+}
+
+func createBooks(c *gin.Context) {
+	var newBook book
+	if err:= c.BindJSON(&newBook);err!=nil{
+		return
+	}
+	books = append(books,newBook)
+	c.IndentedJSON(http.StatusCreated,newBook)
+}
+func checkoutBook(c *gin.Context){
+	id, ok := c.GetQuery("id")
+	if !ok{
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message":"Missing id query param"})
+		return
+	}
+
+	book, err:= getBookById(id)
+	if err!= nil{
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message":"Book not found."})
+		return
+	}
+
+	if book.Quantity <=0{
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message":"Book not available"})
+	}
+	book.Quantity -=1
+	c.IndentedJSON(http.StatusOK, book)
+}
+func returnBook(c* gin.Context){
+	id, ok := c.GetQuery("id")
+	if !ok{
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message":"Missing id query param"})
+		return
+	}
+
+	book, err:= getBookById(id)
+	if err!= nil{
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message":"Book not found."})
+		return
+	}
+
+	book.Quantity +=1
+	c.IndentedJSON(http.StatusOK,book)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.GET("/book/:id", bookById)
+	router.POST("/books", createBooks)
+	router.PATCH("/checkout", checkoutBook)
+	router.PATCH("/return",returnBook)
 	router.Run("localhost:8004")
 }
